5d81e3beada25d14ab802e47fc130a34a1ae5d6e
Ahsan <firstsulthangalaxias@gmail.com>
2025-08-28 21:18:13 +0700

    Delete trash files

--- src/app/dashboard/cashflow-statements/inventory-accounting/page.tsx
+++
@@ -1,6 +0,0 @@
-"use client";
-import { AccountingTemplate } from "@/components/templates/AccountingTemplate";
-
-export default function Dashboard() {
-  return <AccountingTemplate formTitle="Inventory Accounting" />;
-}
